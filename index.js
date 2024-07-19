1fbd3c57666cb764bbb38edefc55f0578e309759
Raza <bilgramiraza@yahoo.com>
2024-07-20 04:52:27 +0530

    Setup Skeleton for Api

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('hello World');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (request, response) => {
+	response.send('<h1>PhoneBook Api</h1>');
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+	console.log(`Server Running and Listening on Port ${PORT}`)
+});
