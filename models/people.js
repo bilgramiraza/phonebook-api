6fd6cd1f96f5bb49fa28fa31a924e8844f892fed
Raza <bilgramiraza@yahoo.com>
2024-07-25 17:31:11 +0530

    Setup Mongoose for the project

---
+++ models/people.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', false);
+
+const url = process.env.MONGODB_URI;
+
+mongoose.connect(url)
+	.then(_result => {
+		console.log('Connected to MongoDB')
+	})
+	.catch(error => {
+		console.log(`Error Connecting to MongoDB: ${error.message}`)
+	});
+
+const personSchema = new mongoose.Schema({
+	name: String,
+	number: String,
+});
+
+personSchema.set('toJSON', {
+	transform: (_document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString();
+		delete returnedObject._id;
+		delete returnedObject.__v;
+	}
+});
+
+module.exports = mongoose.model('person', personSchema);
